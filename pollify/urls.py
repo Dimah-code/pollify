f576a9dac342db6edf2a4cad9bebd8da05a27f03
dimah <dimah.code@gmail.com>
2025-12-11 11:06:08 +0330

    chore: change project name from mysite to pollify

--- pollify/urls.py
+++ pollify/urls.py
@@ -1,8 +1,7 @@
-from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls/', include('polls.urls')),
-] + debug_toolbar_urls()
+    path('', include('polls.urls')),
+]
